7d78f783d12f52fb5f65f7d292fc1f29f7059861
mcsandovall <rasec_oiram@live.com.mx>
2022-01-26 20:16:16 +0000

    server create implemented

--- MP1-SkeletonCode/crsd.c
+++ MP1-SkeletonCode/crsd.c
@@ -46,7 +46,7 @@ the chat room membership accordingly.
 /**
  * Functions for the server to talk with the client
 */
-int create_room(const char * room_name);
+int create_room(const char * room_name, const int server_socket);
 int join_room(const char * room_name);
 int delete_room(const char * room_name);
 chat_room_t * search(const char * room_name);
@@ -54,10 +54,11 @@ chat_room_t * search(const char * room_name);
 // Database of rooms 
 chat_room_t room_db[MAX_ROOM];
 int num_rooms = 0;
+int port_number;
 
 int main(int argc, char** argv){
     
-    int port_number = atoi(argv[1]);
+    port_number = atoi(argv[1]);
     int server_socket, client_socket;
     struct sockaddr_in server, client;
     server.sin_family = AF_INET;
@@ -74,22 +75,83 @@ int main(int argc, char** argv){
         exit(EXIT_FAILURE);
     }
 
-    if(listen(server_socket,10) < 0){
+    if(listen(server_socket,20) < 0){
         perror("Server: listen");
         exit(EXIT_FAILURE);
     }
 
     while(1){
-        // client connects to a 
+        if(client_socket = accept(server_socket, (struct sockaddr *) &server, sizeof(server)) < 0){
+            perror("server: accep");
+        }
     }
     return 0;
 }
 
-int create_room(const char * room_name){
-    return -1;
+int create_room(const char * room_name, const int client_socket){
+    // send a reply to the client
+    struct Reply reply;
+    reply.status = SUCCESS;
+    
+    // check if room exist in the databse
+    chat_room_t * room =  search(room_name);
+    if(room != NULL){
+        reply.status = FAILURE_ALREADY_EXISTS;
+        if(send(client_socket, &reply, sizeof(reply), 0) < 0){
+            perror("server: message unsent");
+            return -1;
+        }
+        return -1;
+    }
+    
+    // open a new master socket
+    int sockfd;;
+    struct sockaddr_in socket;
+    socket.sin_family = AF_INET;
+    socket.sin_addr.s_addr = INADDR_ANY;
+    socket.sin_port = htons(++port_number);
+    
+    if(sockfd = socket(AF_INET, SOCK_STREAM, 0) < 0){
+        reply.status = FAILURE_UNKNOWN;
+        perror("server: create the socket");
+        
+    }
+    
+    if(bind(sockfd, (struct sockaddr *) &server, sizeof(server)) < 0){
+        reply.status = FAILURE_UNKNOWN;
+        perror("Server: bind");
+        
+    }
+    
+    // make the a thread wait for the clients to connect
+    
+    // create a new entry in the db if the all the process where successful
+    if(reply.result == SUCCESS){
+        chat_room_t new_room;
+        new_room.port_number = port_number;
+        new_room.num_members = 0;
+        strcpy(new_room.name,room_name);
+        room_db[num_rooms] = new_room;
+        ++num_rooms;
+    }
+    
+    if(send(client_socket, &reply, sizeof(reply), 0) < 0){
+        perror("server: message unsent");
+        return -1;
+    }
+    
+    return sockfd;
 }
 
 int join_room(const char * room_name){
+    // create a reply for the server to send in case of the command 
+    struct Reply reply;
+    reply.status = ;
+    // check if the room exist
+    chat_room_t * room  = search(room_name);
+    if(room == NULL){
+        
+    }
     return -1;
 }
 
