8bc262b9a25dfa275a0b709090bfc0fd7eaf7ba7
Mario Sandoval <87781937+mcsandovall@users.noreply.github.com>
2022-05-01 17:18:30 -0500

    add

---
+++ MP5/TweetCount.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class TweetCount {
+
+  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text(); // this is the word to which assign the values
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String line = value.toString(); // this gets the the whole line from the file
+      
+      if(line.startsWith("T")){ // get only the time for the timeline
+        // get the time only
+        StringTokenizer it = new StringTokenizer(line);
+        int index =  0;
+        while(it.hasMoreTokens()){
+            String wrd = it.nextToken();
+            if(index++ == 2){ // gets the time from the line
+                word.set(wrd.substring(0,2));
+                context.write(word,one);
+            }
+        }
+      }
+    }
+  }
+
+  public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "tweet count");
+    job.setJarByClass(TweetCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
